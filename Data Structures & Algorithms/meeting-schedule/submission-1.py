5d31064784fc4ad5b94ffa460d82650666801828
Jin Kim <jinkimcs@gmail.com>
2026-08-29 23:13:48 -0400

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,19 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+
+        last = 0
+        for interval in intervals:
+            if interval.start < last:
+                return False
+            last = interval.end
+        
+        return True
