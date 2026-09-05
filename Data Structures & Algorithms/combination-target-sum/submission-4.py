728ae8016ba14e5a5797e014951fa35ffac1fc1d
Jin Kim <jinkimcs@gmail.com>
2026-09-04 20:13:02 -0400

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        combs = []
+        comb = []
+
+        def backtrack(start, remain):
+            if remain == 0:
+                combs.append(comb[:])
+            
+            for i in range(start, len(nums)):
+                # if i > start and nums[i] == nums[i - 1]:
+                #     continue
+                if nums[i] > remain:
+                    break
+                comb.append(nums[i])
+                backtrack(i, remain - nums[i])
+                comb.pop()
+
+        backtrack(0, target)
+        return combs
